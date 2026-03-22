6e83c8118c543d85893abaa20a7137dff0038d1d
paularias652 <paularias652@gmail.com>
2026-03-22 11:05:13 -0500

    Creacion del mini proyecto en C++

---
+++ deber.cpp
@@ -0,0 +1,13 @@
+/*
+Mini Proyecto c++
+Author: Paul Arias
+
+Enunciado: Se ha realizado un sistema de registro de notas en consola, esta elaborado con menu, 
+arreglos, funciones, validaciones, procesamiento y presentacion clara. Clasifica al estudiante 
+segun su promedio.
+
+
+
+
+
+*/
