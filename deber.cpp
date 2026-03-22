90546a84055ffd39964e3d2dc8bc94f609e2525c
paularias652 <paularias652@gmail.com>
2026-03-22 11:06:37 -0500

    Declaracion de los arreglos

--- deber.cpp
+++ deber.cpp
@@ -11,3 +11,20 @@ segun su promedio.
 
 
 */
+
+#include <iostream>
+#include <string>
+#include <limits>
+
+using namespace std;
+
+const int MAX_ESTUDIANTES = 30;
+const int MAX_NOTAS = 3;
+
+// Arreglos principales
+string nombres[MAX_ESTUDIANTES];
+float notas[MAX_ESTUDIANTES][MAX_NOTAS];
+float promedios[MAX_ESTUDIANTES];
+bool registroActivo[MAX_ESTUDIANTES] = {false};
+
+int cantidadEstudiantes = 0;
